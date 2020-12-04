bfed3f1ae2377689efe1d7b63436fb2a1d8bb31c
Haleem Hawkins <62164901+acehawk96@users.noreply.github.com>
2020-12-04 00:46:22 -0500

    Two dimensional array added
    Two dimensional array added for tiles.

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -8,13 +8,19 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseButton;
 import javafx.scene.Group;
+import java.io.*; 
+import java.util.*; 
 /*
    
 */
 public class GameBoard
 {     
-   //Varible. 
+   //Varible and fields. 
    int count = 0;
+   int counter = 0;
+   String symbol;
+
+   //GameTiles[][] tiles;
 
    /*
       Build Stage.
@@ -30,16 +36,41 @@ public class GameBoard
       Button endButton = new Button("End Game");
       HBox leftHbox = new HBox();
       HBox rightHbox = new HBox();
+      
       Button button1 = new Button("Test 1");
       Button button2 = new Button("Test 2");
       Button button3 = new Button("Test 3");
       Button button4 = new Button("Test 4");
-      Button button5 = new Button("Test 5");
+      Button button5 = new Button("Test 5");       
       Button button6 = new Button("Test 6");
       Button button7 = new Button("Test 7");
       Button button8 = new Button("Test 8");
       Button button9 = new Button("Test 9");
       
+      /*    Testing  (put into a method maybe?)   */
+      //Make tiles.
+      Button[][] tiles = new Button[3][3];
+      //Build tiles with loop (3 * 3).
+      for(int row = 0; row < tiles.length; row++) 
+      {  
+         for (int col = 0; col < tiles[row].length; col++) 
+         {
+            tiles[row][col] = new Button("tile [" + row + "][" + col + "]");
+            //Add CSS as tiles are made (Time Saver!).
+            tiles[row][col].getStyleClass().addAll("gameButton");
+            //console test for 9 Button objects made.
+            System.out.println(tiles[row][col] + "\t");
+         }
+         System.out.println("------End of Row------ ");
+      }
+      System.out.println("-----Completed creating 9 Buttons with for loop!----- ");
+      
+      /*more testing.
+      for(Button t : tiles)
+      {
+         System.out.println(Arrays.toString(tiles));
+      }
+      */
       
       //Build Board.
       gameBorder.setCenter(centerGrid);
@@ -47,7 +78,7 @@ public class GameBoard
       gameBorder.setLeft(leftHbox);
       gameBorder.setRight(rightHbox);
       
-      
+      /*
       //Center Border Grid Layout (Use to add O's and X's later).
       centerGrid.add(button1, 0, 0);
       centerGrid.add(button2, 1, 0);
@@ -58,6 +89,18 @@ public class GameBoard
       centerGrid.add(button7, 0, 2);
       centerGrid.add(button8, 1, 2);
       centerGrid.add(button9, 2, 2);
+      */
+      
+      //Center Border Grid Layout (Use to add O's and X's later).
+      centerGrid.add(tiles[0][0], 0, 0);
+      centerGrid.add(tiles[1][0], 1, 0);
+      centerGrid.add(tiles[2][0], 2, 0);
+      centerGrid.add(tiles[0][1], 0, 1);
+      centerGrid.add(tiles[1][1], 1, 1);
+      centerGrid.add(tiles[2][1], 2, 1);
+      centerGrid.add(tiles[0][2], 0, 2);
+      centerGrid.add(tiles[1][2], 1, 2);
+      centerGrid.add(tiles[2][2], 2, 2);
       
       //Bottom Border Menu.
       bottomGrid.add(restartButton, 0, 0);
@@ -72,16 +115,18 @@ public class GameBoard
       
       //Set Alignment.
       centerGrid.setAlignment(Pos.CENTER);
+      
       //Add CSS.
-      button1.getStyleClass().addAll("gameButton");
+      /*                   
       button2.getStyleClass().addAll("gameButton");
       button3.getStyleClass().addAll("gameButton");
       button4.getStyleClass().addAll("gameButton");
-      button5.getStyleClass().addAll("gameButton");
+      button5.getStyleClass().addAll("gameButton");      delete buttons
       button6.getStyleClass().addAll("gameButton");
       button7.getStyleClass().addAll("gameButton");
       button8.getStyleClass().addAll("gameButton");
       button9.getStyleClass().addAll("gameButton");
+      */
       bottomGrid.getStyleClass().addAll("pane");
       scene2.getStylesheets().add("css/style.css");
       
@@ -100,62 +145,91 @@ public class GameBoard
       //endButton Actions.
       endButton.setOnAction(event -> secondaryStage.close());
       
-      //button#'s actions to change from X to O.
-      //button1
-      button1.setOnAction(event -> 
-      {  
-         count++;
-         button1.setText(symbol(Integer.toString(count)));
-      });
-      //button2
-      button2.setOnAction(event -> 
-      {  
-         count++;
-         button2.setText(symbol(Integer.toString(count)));
-      });
-      //button3
-      button3.setOnAction(event -> 
-      {  
-         count++;
-         button3.setText(symbol(Integer.toString(count)));
-      });
-      //button4
-      button4.setOnAction(event -> 
-      {  
-         count++;
-         button4.setText(symbol(Integer.toString(count)));
-      });
-      //button5
-      button5.setOnAction(event -> 
-      {  
-         count++;
-         button5.setText(symbol(Integer.toString(count)));
-      });
-      //button6
-      button6.setOnAction(event -> 
-      {  
-         count++;
-         button6.setText(symbol(Integer.toString(count)));
-      });
-      //button7
-      button7.setOnAction(event -> 
-      {  
-         count++;
-         button7.setText(symbol(Integer.toString(count)));
-      });
-      //button8
-      button8.setOnAction(event -> 
-      {  
-         count++;
-         button8.setText(symbol(Integer.toString(count)));
-      });
-      //button9
-      button9.setOnAction(event -> 
-      {  
-         count++;
-         button9.setText(symbol(Integer.toString(count)));
-      });
+      /*button#'s actions to change from X to O.
+      (Some how this defies the law of physics with passing an int and recieving a string "x" or "o").*/
+         //button1
+         tiles[0][0].setOnAction(event ->                                        /*<<<<---------------fix tabbing*/
+         {  
+            count++;
+            counter++;
+            //track number of moves.                                                
+            System.out.println("Number of moves iterated: " + counter);
+            tiles[0][0].setText(symbol(Integer.toString(count)));
+         });
+         //button2
+         tiles[1][0].setOnAction(event -> 
+         {                                                                       /*<<<<---------------fix tabbing*/
+            count++;
+            counter++;
+            //track number of moves.
+            System.out.println("Number of moves iterated: " + counter);
+            tiles[1][0].setText(symbol(Integer.toString(count)));
+         });
+         //button3
+         tiles[2][0].setOnAction(event -> 
+         {  
+            count++;
+            counter++;
+            //track number of moves.
+            System.out.println("Number of moves iterated: " + counter);
+            tiles[2][0].setText(symbol(Integer.toString(count)));
+         });
+         //button4
+         tiles[0][1].setOnAction(event -> 
+         {                                                                                   /*<<<<---------------fix tabbing*/
+            count++;
+            counter++;
+            //track number of moves.
+            System.out.println("Number of moves iterated: " + counter);
+            tiles[0][1].setText(symbol(Integer.toString(count)));
+         });
+         //button5
+         tiles[1][1].setOnAction(event -> 
+         {  
+            count++;
+            counter++;
+            //track number of moves.
+            System.out.println("Number of moves iterated: " + counter);
+            tiles[1][1].setText(symbol(Integer.toString(count)));
+         });
+         //button6
+         tiles[2][1].setOnAction(event -> 
+         {  
+            count++;
+            counter++;
+            //track number of moves.
+            System.out.println("Number of moves iterated: " + counter);                               /*<<<<---------------fix tabbing*/
+            tiles[2][1].setText(symbol(Integer.toString(count)));
+         });
+         //button7
+         tiles[0][2].setOnAction(event -> 
+         {  
+            count++;
+            counter++;
+            //track number of moves.
+            System.out.println("Number of moves iterated: " + counter);
+            tiles[0][2].setText(symbol(Integer.toString(count)));
+         });
+         //button8
+         tiles[1][2].setOnAction(event -> 
+         {  
+            count++;
+            counter++;
+            //track number of moves.
+            System.out.println("Number of moves iterated: " + counter);
+            tiles[1][2].setText(symbol(Integer.toString(count)));
+         });
+         //button9
+         tiles[2][2].setOnAction(event -> 
+         {  
+            count++;
+            counter++;
+            //track number of moves.
+            System.out.println("Number of moves iterated: " + counter);
+            tiles[2][2].setText(symbol(Integer.toString(count)));
+         });                                                                                 /*<<<<---------------fix tabbing*/
    } 
+   
    
    /*
    */
@@ -166,7 +240,7 @@ public class GameBoard
       int turn = Integer.parseInt(sym);
       //Set to X.
       if (turn % 2 == 0)
-      {
+      {  //maybe an image obj.
          sym = "X";     
       }
       //Set to O.
