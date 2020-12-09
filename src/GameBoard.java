b84832e44b7106e360dcd9a584e7110fbe420cb1
Haleem Hawkins <62164901+acehawk96@users.noreply.github.com>
2020-12-08 22:16:50 -0500

    Almost done!
    Almost done!

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -102,6 +102,7 @@ public class GameBoard
       
       //Set Alignment.
       centerGrid.setAlignment(Pos.CENTER);
+      gameBorder.setAlignment(alert, Pos.CENTER);
       
       //Add CSS.
       bottomGrid.getStyleClass().addAll("pane");
